46b38e3a323dbc67202419dce08a645e2a4734aa
Yuriy Bogomolov <yuriy.bogomolov@gmail.com>
2026-02-24 17:28:51 +0200

    Revert "PWA shenanigans"
    This reverts commit e4a1a313353bd83f1d205fdf5e8780f3fc8b7b31.

--- vite.config.ts
+++ vite.config.ts
@@ -1,46 +1,13 @@
-import tailwindcss from "@tailwindcss/vite";
-import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 import { VitePWA } from "vite-plugin-pwa";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
   base: "/5pfh",
-  plugins: [
-    react(),
-    VitePWA({
-      registerType: "autoUpdate",
-      includeAssets: [
-        "favicon.ico",
-        "apple-touch-icon.png",
-        "android-chrome-192x192.png",
-        "android-chrome-512x512.png",
-      ],
-      manifest: {
-        name: "Five Parsecs From Home Companion",
-        short_name: "5PFHC",
-        icons: [
-          {
-            src: "/android-chrome-192x192.png",
-            sizes: "192x192",
-            type: "image/png",
-          },
-          {
-            src: "/android-chrome-512x512.png",
-            sizes: "512x512",
-            type: "image/png",
-          },
-        ],
-        theme_color: "#14a0e6",
-        background_color: "#000913",
-        start_url: "https://ybogomolov.me/5pfh",
-        display: "standalone",
-        display_override: ["window-controls-overlay"],
-      },
-    }),
-    tailwindcss(),
-  ],
+  plugins: [react(), VitePWA({ registerType: "autoUpdate" }), tailwindcss()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
